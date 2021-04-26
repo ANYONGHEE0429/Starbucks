382a5ed70785d074f5228e886ce9cd0ad8ae295b
YongheeAn <ayh0429@gmail.com>
2021-04-26 17:39:24 +0900

    vertical swiper

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const badge = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function(){
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if(window.scrollY > 500) {
     // badge.style.display = 'none';
     // gsap.to(요소, 지속시간, 옵션);
@@ -17,3 +17,22 @@ window.addEventListener('scroll', _.throttle(function(){
     });
   }
 }, 300));
+//_.throttle(함수, 시간)
+
+
+const fade = document.querySelectorAll('.visual .fade-in');
+fade.forEach(function (fade, index) {
+  //gsap.to(요소, 지속시간, 옵션)
+  gsap.to(fade, 1, {
+    //몇 초 뒤에 실행될건지
+    delay: (index + 1) * .7,  //0.7, 1.4, 2.1, 2.8
+    opacity: 1
+  });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
